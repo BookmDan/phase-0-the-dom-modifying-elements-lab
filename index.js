9365b0bf2e786de4c71ed4d9458857a71370aa16
BookmDan <danoh329@gmail.com>
2023-07-02 17:56:20 -0700

    dom?

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+  const newHeader = document.createElement('h1');
+  newHeader.textContent = "Daniel Oh is the champion";
+  newHeader.id = "victory";
+ 
